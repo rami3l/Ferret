08d1bfefce142176d4178af2acda2a384a732836
rami3l <rami3l@outlook.com>
2022-01-14 16:35:13 +0100

    refactor: rename `fr.ferret.model` to `fr.ferret.model.legacy`

--- src/main/java/fr/ferret/model/legacy/GeneModel.java
+++ src/main/java/fr/ferret/model/legacy/GeneModel.java
@@ -2,16 +2,14 @@
  * To change this license header, choose License Headers in Project Properties. To change this
  * template file, choose Tools | Templates and open the template in the editor.
  */
-package fr.ferret.model;
+package fr.ferret.model.legacy;
 
 /**
  *
  * @author youne
  */
-public class VariantModel extends ElementSaisiModel {
-
-    public VariantModel(String fileAdresse, String[] population) {
+public class GeneModel extends ElementSaisiModel {
+    public GeneModel(String fileAdresse, String[] population) {
         super(fileAdresse, population);
     }
-
 }
--- src/main/java/fr/ferret/model/legacy/NCBIData.java
+++ src/main/java/fr/ferret/model/legacy/NCBIData.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties. To change this
  * template file, choose Tools | Templates and open the template in the editor.
  */
-package fr.ferret.model;
+package fr.ferret.model.legacy;
 
 import java.io.IOException;
 import java.util.ArrayList;
