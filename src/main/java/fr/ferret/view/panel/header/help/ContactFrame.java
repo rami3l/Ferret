a8627077c04ae64de3cd4118fa23355a52976882
Jean Tissot <asphjt@gmail.com>
2022-01-24 20:08:56 +0100

    contact frame refactored

--- src/main/java/fr/ferret/view/panel/header/help/ContactFrame.java
+++ src/main/java/fr/ferret/view/panel/header/help/ContactFrame.java
@@ -9,23 +9,31 @@ import javax.swing.JTextArea;
 import fr.ferret.utils.Resource;
 
 public class ContactFrame extends JFrame {
+
     public ContactFrame() {
+
         super(Resource.getTextElement("contact.title"));
 
+        // panel settings
         JPanel contactPanel = new JPanel();
-        JLabel contactPeopleLabel = new JLabel(Resource.getTextElement("contact.text"));
-        JTextArea contactEmailLabel = new JTextArea(Resource.getTextElement("contact.mail"));
-
-        this.getContentPane().add(contactPanel);
-        this.setResizable(true);
-        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         contactPanel.setLayout(new BoxLayout(contactPanel, BoxLayout.Y_AXIS));
         contactPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+        // Contact names
+        JLabel contactPeopleLabel = new JLabel(Resource.getTextElement("contact.text"));
         contactPeopleLabel.setAlignmentX(CENTER_ALIGNMENT);
         contactPanel.add(contactPeopleLabel);
+
+        // Contact emails
+        JTextArea contactEmailLabel = new JTextArea(Resource.getTextElement("contact.mail"));
         contactEmailLabel.setAlignmentX(CENTER_ALIGNMENT);
         contactEmailLabel.setBackground(contactPanel.getBackground());
         contactPanel.add(contactEmailLabel);
+
+        // frame settings
+        this.getContentPane().add(contactPanel);
+        this.setResizable(true);
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         this.pack();
     }
 }
