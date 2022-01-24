0da021037956cccb51d025bc32e9bd7dc2ed8f02
Jean Tissot <asphjt@gmail.com>
2022-01-24 20:05:48 +0100

    about frame refactored

--- src/main/java/fr/ferret/view/panel/header/help/AboutFrame.java
+++ src/main/java/fr/ferret/view/panel/header/help/AboutFrame.java
@@ -12,35 +12,48 @@ import fr.ferret.utils.Resource;
 import fr.ferret.view.LinkLabel;
 
 public class AboutFrame extends JFrame {
+
     public AboutFrame() {
+
         super(Resource.getTextElement("about.title"));
+
         JPanel aboutPanel = new JPanel();
-        JLabel ferretVersionLabel = new JLabel(Resource.getTextElement("about.version"));
-        JLabel ferretDateLabel = new JLabel(Resource.getTextElement("about.date"));
-        JTextArea ferretCitation = new JTextArea(Resource.getTextElement("about.citation"), 4, 50);
-
-        LinkLabel ferretWebLabelAbout = new LinkLabel(Resource.getTextElement("about.link"));
-
-        this.getContentPane().add(aboutPanel);
-        this.setResizable(true);
-        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         aboutPanel.setLayout(new BoxLayout(aboutPanel, BoxLayout.Y_AXIS));
         aboutPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+        // Version label
+        JLabel ferretVersionLabel = new JLabel(Resource.getTextElement("about.version"));
         aboutPanel.add(ferretVersionLabel);
+
+        // Date label
+        JLabel ferretDateLabel = new JLabel(Resource.getTextElement("about.date"));
         aboutPanel.add(ferretDateLabel);
+
         aboutPanel.add(Box.createRigidArea(new Dimension(0, 12)));
-        ferretWebLabelAbout.setBackgroundColor(aboutPanel.getBackground());
-        ferretWebLabelAbout.init();
-        ferretWebLabelAbout.setAlignmentX(LEFT_ALIGNMENT);
-        ferretWebLabelAbout.setMaximumSize(ferretWebLabelAbout.getPreferredSize());
-        aboutPanel.add(ferretWebLabelAbout);
+
+        // About link
+        LinkLabel ferretAboutLink = new LinkLabel(Resource.getTextElement("about.link"));
+        ferretAboutLink.setBackgroundColor(aboutPanel.getBackground());
+        ferretAboutLink.init();
+        ferretAboutLink.setAlignmentX(LEFT_ALIGNMENT);
+        ferretAboutLink.setMaximumSize(ferretAboutLink.getPreferredSize());
+        aboutPanel.add(ferretAboutLink);
+
         aboutPanel.add(Box.createRigidArea(new Dimension(0, 12)));
+
+        // Citation
+        JTextArea ferretCitation = new JTextArea(Resource.getTextElement("about.citation"), 4, 50);
         ferretCitation.setAlignmentX(LEFT_ALIGNMENT);
         ferretCitation.setLineWrap(true);
         ferretCitation.setWrapStyleWord(true);
         ferretCitation.setBackground(aboutPanel.getBackground());
         ferretCitation.setMaximumSize(ferretCitation.getPreferredSize());
         aboutPanel.add(ferretCitation);
+
+        // frame settings
+        this.getContentPane().add(aboutPanel);
+        this.setResizable(true);
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         this.pack();
     }
 }
