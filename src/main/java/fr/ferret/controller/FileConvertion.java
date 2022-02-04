d6248786384571e72d821d5751f6f733ec887cba
BlueCloud02 <comptes.98@gmail.com>
2022-02-04 09:41:21 +0100

    Create convertion methods.

--- src/main/java/fr/ferret/controller/FileConvertion.java
+++ src/main/java/fr/ferret/controller/FileConvertion.java
@@ -7,6 +7,9 @@ import java.util.*;
  */
 public class FileConvertion {
 
+    /**
+     * List of the possible extentions of files, without dot.
+     */
     private ArrayList<String> fileExtensions;
 
     public FileConvertion() {
@@ -16,5 +19,36 @@ public class FileConvertion {
         this.fileExtensions.add("map");
         this.fileExtensions.add("ped");
         this.fileExtensions.add("info");
+    }
+
+    /**
+     * Read the given VCF file and create the corresponding FRQ file, returning its relative path.
+     * 
+     * @param vcfFilePath relative path to the VCF file we want to convert.
+     */
+    public String convertVcfToFrq(String vcfFilePath) {
+        // TODO.
+        return vcfFilePath;
+    }
+
+    /**
+     * Read the given VCF file and create the corresponding MAP and PED files, returning their
+     * relative path.
+     * 
+     * @param vcfFilePath relative path to the VCF file we want to convert.
+     */
+    public String convertVcfToMapAndPed(String vcfFilePath) {
+        // TODO.
+        return vcfFilePath;
+    }
+
+    /**
+     * Read the given VCF file and create the corresponding INFO file, returning its relative path.
+     * 
+     * @param vcfFilePath relative path to the VCF file we want to convert.
+     */
+    public String convertVcfToInfo(String vcfFilePath) {
+        // TODO.
+        return vcfFilePath;
     }
 }
