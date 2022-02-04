5ce065a24f817e93f5e6b9f11dfa6977306b4612
rami3l <rami3l@outlook.com>
2022-02-04 09:48:18 +0100

    test: complete `TestIgsrClient`

--- src/test/java/fr/ferret/model/TestIgsrClient.java
+++ src/test/java/fr/ferret/model/TestIgsrClient.java
@@ -3,12 +3,14 @@ package fr.ferret.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 
 public class TestIgsrClient {
     @Test
-    public void testBasicQuery() {
+    public void testBasicQuery() throws IOException {
         var chr = 1;
         var start = 196194909;
         var end = 196577570;
@@ -16,9 +18,11 @@ public class TestIgsrClient {
         try (var reader = igsrClient.reader();) {
             var it = reader.query(Integer.toString(chr), start, end);
             assertNotEquals(null, it);
-            assertEquals("what?", it.next());
-        } catch (IOException e) {
-            // TODO: Handle this exception
+            // #CHROM POS ID REF ALT QUAL FILTER INFO
+            // Split whitespaces in the spec line.
+            var fields = Arrays.asList(it.next().split("\\s+"));
+            var expected = List.of("1", "196187886", ".", "T", "<CN2>", "100", "PASS");
+            assertEquals(expected, fields.subList(0, expected.size()));
         }
     }
 }
