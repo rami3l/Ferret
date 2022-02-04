0a67c109a26f273e6c30c34148a5c674a3a2ebd7
rami3l <rami3l@outlook.com>
2022-02-04 17:27:41 +0100

    docs: add explanation in `TestIgsrClient`

--- src/main/java/fr/ferret/model/IgsrClient.java
+++ src/main/java/fr/ferret/model/IgsrClient.java
@@ -10,14 +10,18 @@ import lombok.Builder;
  */
 @Builder
 public class IgsrClient {
-
+    /**
+     * The chromosome number, eg. `1`.
+     */
     private int chromosome;
 
+    // TODO: Change this to constant aliases.
     @Builder.Default
     private String fileUrlFormat =
             "http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr{0,number}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz";
 
     private String getFilePath() {
+        // Replace chromosome number in the template string.
         return MessageFormat.format(fileUrlFormat, chromosome);
     }
 
@@ -25,6 +29,7 @@ public class IgsrClient {
         return getFilePath() + ".tbi";
     }
 
+    /** Returns a new {@code TabixReader} instance pointing to the given file path. */
     public TabixReader reader() throws IOException {
         return new TabixReader(getFilePath(), getIndexPath());
     }
--- src/test/java/fr/ferret/model/TestIgsrClient.java
+++ src/test/java/fr/ferret/model/TestIgsrClient.java
@@ -18,11 +18,15 @@ public class TestIgsrClient {
         try (var reader = igsrClient.reader();) {
             var it = reader.query(Integer.toString(chr), start, end);
             assertNotEquals(null, it);
-            // #CHROM POS ID REF ALT QUAL FILTER INFO
-            // Split whitespaces in the spec line.
+            // `it.next()` is the next line in the iterator.
+            // Split whitespaces in the spec line to get all fields.
             var fields = Arrays.asList(it.next().split("\\s+"));
+            // Fixed fields:
+            // #CHROM POS ID REF ALT QUAL FILTER INFO
+            // https://samtools.github.io/hts-specs/VCFv4.2.pdf
             var expected = List.of("1", "196187886", ".", "T", "<CN2>", "100", "PASS");
             assertEquals(expected, fields.subList(0, expected.size()));
+            // Optional fields: see the link above.
         }
     }
 }
