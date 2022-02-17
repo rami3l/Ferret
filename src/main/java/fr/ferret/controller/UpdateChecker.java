7a7aa2676a366638072643573c9f3a72bc7c19fd
Jean Tissot <asphjt@gmail.com>
2022-02-18 00:11:05 +0100

    Refactor UpdateChecker

--- src/main/java/fr/ferret/controller/UpdateChecker.java
+++ src/main/java/fr/ferret/controller/UpdateChecker.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import javax.swing.SwingWorker;
+
+import fr.ferret.utils.Resource;
 import lombok.Getter;
 
 /**
@@ -19,29 +21,28 @@ public class UpdateChecker extends SwingWorker<Boolean, Object> {
 
     @Override
     protected Boolean doInBackground() throws Exception {
-        try {
-            // TODO Update links and tests
-            URL urlLocation =
-                    new URL("https://webspace.princeton.edu/users/taverner/updateFerret.txt");
-            BufferedReader br = new BufferedReader(new InputStreamReader(urlLocation.openStream()));
+
+        // TODO Update links and tests
+        URL urlLocation = new URL(Resource.getServerConfig("update.url"));
+
+        try(var br = new BufferedReader(new InputStreamReader(urlLocation.openStream()))) {
             String currentString;
             while ((currentString = br.readLine()) != null) {
                 String[] updateInformation = currentString.split(":");
-                if (Integer.parseInt(updateInformation[0]) <= 3) { // 2 is the internal version
-                                                                   // number of this version of
-                                                                   // Ferret
-                    if (updateInformation[1].equals("urgentUpdate")) {
-                        updateMessage =
-                                "Urgent update. Ferret may not be functional until you update: "
-                                        + updateInformation[2];
+                // 2 is the internal version number of this version of Ferret
+                if (Integer.parseInt(updateInformation[0]) <= 3) {
+                    if ("urgentUpdate".equals(updateInformation[1])) {
+                        updateMessage = Resource.getTextElement("update.urgent") + " "
+                            + updateInformation[2];
                         needUpdate = true;
                         urgentUpdate = true;
-                    } else if (updateInformation[1].equals("recommendedUpdate")) {
-                        updateMessage = "Recommended update: " + updateInformation[2];
+                    } else if ("recommendedUpdate".equals(updateInformation[1])) {
+                        updateMessage = Resource.getTextElement("update.recommended") + " "
+                            + updateInformation[2];
                         needUpdate = true;
                         urgentUpdate = false;
-                    } else if (updateInformation[1].equals("noUpdate")) {
-                        updateMessage = "Ferret is up to date";
+                    } else if ("noUpdate".equals(updateInformation[1])) {
+                        updateMessage = Resource.getTextElement("update.uptodate");
                         needUpdate = false;
                         urgentUpdate = false;
                     }
@@ -50,13 +51,12 @@ public class UpdateChecker extends SwingWorker<Boolean, Object> {
             if (needUpdate == null || urgentUpdate == null) {
                 needUpdate = true;
                 urgentUpdate = true;
-                updateMessage =
-                        "Unable to contact update server. Try again later or update Ferret.";
+                updateMessage = Resource.getTextElement("update.error");
             }
         } catch (IOException e) {
             needUpdate = true;
             urgentUpdate = true;
-            updateMessage = "Unable to contact update server. Try again later or update Ferret.";
+            updateMessage = Resource.getTextElement("update.error");
         }
         return needUpdate;
     }
