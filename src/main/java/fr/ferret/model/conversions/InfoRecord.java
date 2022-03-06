903428527a2f95219a1ff1d8fc6456dd1ef8dd6d
rami3l <rami3l@outlook.com>
2022-03-06 16:03:58 +0100

    refactor(conversions): cleanup `.frq` conversion logic

--- src/main/java/fr/ferret/model/conversions/InfoRecord.java
+++ src/main/java/fr/ferret/model/conversions/InfoRecord.java
@@ -3,7 +3,7 @@ package fr.ferret.model.conversions;
 import javax.annotation.Nonnull;
 import htsjdk.variant.variantcontext.VariantContext;
 
-/** A single line in a {@code .info} file. */
+/** A single line in a {@code .info} file. See: https://www.cog-genomics.org/plink2/formats#info */
 public record InfoRecord(@Nonnull String variantId, int position) {
     public InfoRecord(VariantContext ctx) {
         this(VcfConverter.generateVariantId(ctx), ctx.getStart());
