6cfefe6577a4fbdce2800ed7a919779b524f3c5e
Jean Tissot <asphjt@gmail.com>
2022-03-22 11:53:55 +0100

    Updates VcfConverterTest

--- src/test/java/fr/ferret/model/conversions/VcfConverterTest.java
+++ src/test/java/fr/ferret/model/conversions/VcfConverterTest.java
@@ -1,7 +1,5 @@
 package fr.ferret.model.conversions;
 
-import fr.ferret.model.Phase1KG;
-import fr.ferret.model.SampleSelection;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
@@ -12,74 +10,70 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class VcfConverterTest {
-    private final String chr = "1";
-    //private final int start = 196194909;
-    //private final int end = 196194913;
-    private final Phase1KG phase = new Phase1KG("phase3", "Phase 3");
-    //private final String vcfSource = "src/test/resources/chr1-africans-phase3.vcf.gz";
-    private final String vcfPath = "src/test/resources/chr1-africans-phase3.vcf";
-    // TODO: if we need toRegion::getZones correct this → see IgsrClientTest L72
-    private final SampleSelection selection = new SampleSelection(); //.add("AFR", List.of("MSL"));
 
+    private final String vcfPath = "src/test/resources/1kg/phase3/46370700-46371000-EUR-GBR.vcf";
 
-    //@BeforeAll
-    //void init() {
-    //    new VcfExport(Flux.just(new Locus(chr, start, end))).setFilter(selection)
-    //        .start(new File(vcfPath)).blockLast();
-    //}
+    @Test
+    void testToPed(@TempDir Path tempDir) throws Exception {
+        var outPath = tempDir.resolve("test.ped");
 
-    //@Test
-    //void testToPed(@TempDir Path tempDir) throws Exception {
-    //    var outPath = tempDir.resolve("test.ped");
-    //
-    //    var samples = Resource.getSamples(phase, selection);
-    //    VcfConverter.toPed(vcfPath, outPath.toString());
-    //
-    //    var got = Files.readAllLines(outPath);
-    //    var expected = Stream.of("Y016 NA18523 0 0 2 0 T T T T", "SL11 HG03081 0 0 1 0 T T T T",
-    //            "Y014 NA18520 0 0 2 0 T T T T").map(s -> s.replace(' ', '\t')).toList();
-    //    assertAll(() -> assertEquals(expected, got.subList(0, 3)),
-    //            () -> assertEquals(661, got.size()));
-    //}
+        VcfConverter.toPed(vcfPath, outPath.toString());
+
+        var got = Files.readAllLines(outPath);
+        var expected =
+            Stream.of("HG00097 HG00097 0 0 2 0 G G C C A G T T G G G G A A A A T T T T G G",
+                    "HG00130 HG00130 0 0 2 0 G G C C G A T T G G G G A A A A T T T T G G",
+                    "HG00251 HG00251 0 0 1 0 G G C C G G T T G G G G A A A A T T T T G C",
+                    "HG00096 HG00096 0 0 1 0 G G C C A A T T G G G G A A A A T T T T G G",
+                    "HG00250 HG00250 0 0 2 0 G G C C A A T T G G G G A A A A T T T T G G")
+                .map(s -> s.replace(' ', '\t')).toList();
+        assertAll(() -> assertEquals(expected, got.subList(0, 5)),
+                () -> assertEquals(91, got.size()));
+    }
 
     @Test
     void testToInfo(@TempDir Path tempDir) throws Exception {
         var outPath = tempDir.resolve("test.info");
 
-        var samples = selection.getSample();
         VcfConverter.toInfo(vcfPath, outPath.toString());
 
         var got = Files.readAllLines(outPath);
-        var expected = Stream.of("1:196187886 196187886", "1:196194911 196194911")
+        var expected = Stream.of("3:46370719 46370719", "3:46370803 46370803",
+                "3:46370804 46370804", "3:46370830 46370830", "3:46370885 46370885",
+                "3:46370888 46370888", "3:46370901 46370901", "3:46370919 46370919",
+                "3:46370923 46370923", "3:46370974 46370974", "3:46370978 46370978")
                 .map(s -> s.replace(' ', '\t')).toList();
-        assertAll(() -> assertEquals(expected, got), () -> assertEquals(2, got.size()));
+        assertAll(() -> assertEquals(expected, got), () -> assertEquals(11, got.size()));
     }
 
     @Test
     void testToMap(@TempDir Path tempDir) throws Exception {
         var outPath = tempDir.resolve("test.map");
 
-        var samples = selection.getSample();
         VcfConverter.toMap(vcfPath, outPath.toString());
 
         var got = Files.readAllLines(outPath);
-        var expected = Stream.of("1 1:196187886 0 196187886", "1 1:196194911 0 196194911")
-                .map(s -> s.replace(' ', '\t')).toList();
-        assertAll(() -> assertEquals(expected, got), () -> assertEquals(2, got.size()));
+        var expected = Stream.of("3 3:46370719 0 46370719", "3 3:46370803 0 46370803",
+                "3 3:46370804 0 46370804", "3 3:46370830 0 46370830", "3 3:46370885 0 46370885",
+                "3 3:46370888 0 46370888", "3 3:46370901 0 46370901", "3 3:46370919 0 46370919",
+                "3 3:46370923 0 46370923", "3 3:46370974 0 46370974", "3 3:46370978 0 46370978")
+            .map(s -> s.replace(' ', '\t')).toList();
+        assertAll(() -> assertEquals(expected, got), () -> assertEquals(11, got.size()));
     }
 
-    //@Test
-    //void testToFrq(@TempDir Path tempDir) throws Exception {
-    //    var outPath = tempDir.resolve("test.frq");
-    //
-    //    var samples = Resource.getSamples(phase, selection);
-    //    VcfConverter.toFrq(vcfPath, outPath.toString());
-    //
-    //    var got = Files.readAllLines(outPath);
-    //    var expected = Stream.of("1 1:196187886 T . 1.0000 661", "1 1:196194911 T . 1.0000 661")
-    //        .map(s -> s.replace(' ', '\t')).toList();
-    //    assertAll(() -> assertEquals(expected, got), () -> assertEquals(2, got.size()));
-    //}
+    @Test
+    void testToFrq(@TempDir Path tempDir) throws Exception {
+        var outPath = tempDir.resolve("test.frq");
+
+        VcfConverter.toFrq(vcfPath, outPath.toString());
+
+        var got = Files.readAllLines(outPath);
+        var expected = Stream.of("3 3:46370719 G . 1.0000 91", "3 3:46370803 C T 0.9286 91",
+                "3 3:46370804 G A 0.5000 91", "3 3:46370830 T . 1.0000 91", "3 3:46370885 G . 1.0000 91",
+                "3 3:46370888 G . 1.0000 91", "3 3:46370901 A . 1.0000 91", "3 3:46370919 A . 1.0000 91",
+                "3 3:46370923 T . 1.0000 91", "3 3:46370974 T . 1.0000 91", "3 3:46370978 G C 0.9945 91")
+            .map(s -> s.replace(' ', '\t')).toList();
+        assertAll(() -> assertEquals(expected, got), () -> assertEquals(11, got.size()));
+    }
 }
